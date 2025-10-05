454e4f39482352f2b6c61536063d73e722f6fa05
Nishant Rathore <rnishant609@gmail.com>
2025-10-05 20:49:07 +0530

    initial render time fixed

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -5,14 +5,6 @@ const ProtectedRoute = ({ children }) => {
     const { isAuthenticated, isLoading } = useSelector((state) => state.auth);
     const location = useLocation();
 
-    if (isLoading) {
-        return (
-            <div className="flex justify-center items-center h-screen">
-                <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
-            </div>
-        );
-    }
-
     if (!isAuthenticated && location.pathname === "/lead") {
         return <Navigate to="/" replace />;
     }
